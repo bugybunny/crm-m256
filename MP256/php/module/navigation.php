d08834ff84bef2e73fcb9683554638dcb21700da
marco.syfrig <marco.syfrig@1a3890e3-0b1e-26c5-6cbf-53fb69772ad0>
2010-06-24 07:35:50 +0000

    Suchen hinzugefügt

--- MP256/php/module/navigation.php
+++ MP256/php/module/navigation.php
@@ -3,8 +3,10 @@ $php_self = $_SERVER['PHP_SELF'];
 $output .= $logged_in ? get_link("Logout", $php_self.'?site=logout') : get_link("Login", $php_self.'?site=login');
 $output .= ' | '.get_link("Registration", $php_self.'?site=registrieren');
 $output .= ' | '.get_link("Anfrage erstellen", $php_self.'?site=anfrage_erstellen');
-if($logged_in)
+if($logged_in) {
 	$output .= ' | '.get_link("Eingeloggt als '<b>".$_SESSION['user_name']."</b>'", $php_self.'?site=einstellungen');
+	$output .= ' | '.get_link("Suchen", $php_self.'?site=suchen');
+}
 
 function get_link($text, $url) {
 	return '<a href="'.$url.'">'.$text.'</a>';
